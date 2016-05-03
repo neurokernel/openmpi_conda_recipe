06e6b8011e30b9807aa58351e69f38d5c2132216
Lev Givon <lev@columbia.edu>
2016-05-03 10:06:48 -0400

    Update to 1.10.2.

--- build.sh
+++ build.sh
@@ -3,9 +3,10 @@
 export LD_LIBRARY_PATH=
 ./configure --prefix=$PREFIX --with-cuda=/usr/local/cuda/ \
     --with-threads=posix --with-slurm --with-pmi \
+    --with-pmi-libdir=/usr/lib/x86_64-linux-gnu \
     --disable-silent-rules --with-hwloc=/usr \
     --disable-mca-dso --with-devel-headers --with-sge \
     --enable-heterogeneous --disable-vt \
-#    --with-wrapper-ldflags="-Wl,-rpath -Wl,/usr/lib/x86_64-linux-gnu"
+    --with-wrapper-ldflags="-Wl,-rpath -Wl,/usr/lib/x86_64-linux-gnu"
 make -j $CPU_COUNT
 make install
